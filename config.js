33a56371257eee80985238f816f6f1df5b7c3204
Jan Vlnas <git.noreply@jan.vlnas.cz>
2023-02-03 01:39:15 +0100

    package lock, configurable tweet ID

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
   bearerToken: process.env.TWITTER_BEARER_TOKEN,
   // tweetId: "20",
-  tweetId: "1621026986784337922",
+  tweetId: process.env.TWEET_ID || "1621026986784337922",
 };
